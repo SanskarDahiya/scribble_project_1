b1fd7f062c243183878e05a430ec32db6a1bd789
SanskarDahiya <sanskardahiya98@gmail.com>
2020-06-30 21:40:20 +0530

    add fetch user by id

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,7 +8,7 @@ const { query } = require("express");
 const { encPassword } = require("../apis/encryption");
 const api = getMethods(userDB);
 
-const createUser = async req => {
+const createUser = async (req) => {
   const { data } = req.body;
   let { password, _id } = data;
   if (!data || !_id || !password) {
@@ -21,7 +21,7 @@ const createUser = async req => {
   return await api.addData({ ...data, password });
 };
 
-const validateUser = async req => {
+const validateUser = async (req) => {
   const { username, password } = req.body;
   if (!username || !password) {
     let err = new Error();
@@ -32,9 +32,23 @@ const validateUser = async req => {
   const query = { $and: [{ _id: username }, { password: encPassword(password) }] };
   return await api.getSingleData(query);
 };
+const getUserById = async (req) => {
+  const { _id } = req.body;
 
+  if (!_id) {
+    let err = new Error();
+    err.message = "Insufficient Params";
+    err.code = "Insufficient Params";
+    throw err;
+  }
+  const query = { _id };
+  const result = await api.getSingleData(query);
+  console.log("recived", result);
+  return result || [];
+};
 router.all("/create", serverPrefix(createUser));
 router.all("/validate", serverPrefix(validateUser));
+router.all("/getUserById", serverPrefix(getUserById));
 
 router.all(`/`, (req, res) => {
   res.sendStatus(200);
