e724e7712258317cd850fe5811f9a772b11d95e3
SanskarDahiya <sanskardahiya98@gmail.com>
2020-07-05 12:38:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,36 +7,44 @@ import Index from "./componnents";
 import SendToUserMessage from "./componnents/multiBlog";
 import PAGENOTFOUND from "./componnents/pageNotFound";
 import Login from "./componnents/login";
+
 const localStoragename = "loginCredentials";
 const App = (props) => {
-  // { _id: "5ee0f4e5e081ebe6a207e6f7", username: "admin" }
   const [loginDetails, loginUpdater] = useState(false);
-  // const [loginDetails, loginUpdater] = useState({ _id: "5ee0f4e5e081ebe6a207e6f7", username: "admin" });
+  // const [loginDetails, loginUpdater] = useState({ _id: "kunal", username: "admin" });
   const setLoginDetails = (data) => {
-    // used for login as well as logout
-    loginUpdater(data);
-    if (data) {
-      data["expired"] = new Date().getTime() + 1000 * 60 * 60 * 24;
-      localStorage.setItem(localStoragename, JSON.stringify(data));
-    } else {
-      localStorage.removeItem(localStoragename);
+    try {
+      // used for login as well as logout
+      loginUpdater(data);
+      if (data) {
+        data["expired"] = new Date().getTime() + 1000 * 60 * 60 * 24;
+        localStorage.setItem(localStoragename, JSON.stringify(data));
+      } else {
+        localStorage.removeItem(localStoragename);
+      }
+    } catch (err) {
+      console.error(err);
     }
   };
 
   useEffect(() => {
-    let data = localStorage.getItem(localStoragename);
-    if (data) {
-      data = JSON.parse("" + data);
-      if (data["expired"] && data["expired"] > new Date().getTime()) {
-        loginUpdater(data);
+    try {
+      let data = localStorage.getItem(localStoragename);
+      if (data) {
+        data = JSON.parse("" + data);
+        if (data["expired"] && data["expired"] > new Date().getTime()) {
+          loginUpdater(data);
+        }
       }
+    } catch (err) {
+      console.error(err);
     }
   }, []);
   return (
     <Router>
       <Route path="/" render={(zz) => <Header {...zz} user={loginDetails} userUpdate={setLoginDetails} />} />
       <Switch>
-        <Route path="/" exact render={(myProps) => <Index {...myProps} user={loginDetails} userUpdate={setLoginDetails} />} />
+        <Route path="/" exact render={(myProps) => <Index {...props}{...myProps} user={loginDetails} userUpdate={setLoginDetails} />} />
         <Route path="/login" exact render={(myProps) => <Login {...myProps} user={loginDetails} userUpdate={setLoginDetails} />} />
         <Route path="/user" render={(myProps) => <SendToUserMessage {...myProps} user={loginDetails} userUpdate={setLoginDetails} />} />
         <Route path="*" component={PAGENOTFOUND} />
