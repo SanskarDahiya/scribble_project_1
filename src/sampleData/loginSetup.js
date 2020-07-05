bf8366c11fad4aafdb5fa7e71cf979bf71eb1112
SanskarDahiya <sanskardahiya98@gmail.com>
2020-07-05 10:24:53 +0530

    changes

--- src/sampleData/loginSetup.js
+++ src/sampleData/loginSetup.js
@@ -2,12 +2,8 @@ import Axios from "axios";
 import dotenv from "dotenv";
 const serverUrl = "https://scribble-back.herokuapp.com/"; //"http://localhost:9797/";
 console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-const result = dotenv.config();
-if (result.error) {
-  console.log(result.error, "<<<ERROR ENCOUNTER");
-}
-console.log(result.parsed, "<<<Result Present");
 console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
 export const validateLogin = async ({ username, password }) => {
   if (username && password) {
     return await fetch("user/validate", {
@@ -43,6 +39,14 @@ export const getAllMessages = async (user) => {
 };
 
 const fetch = async (suffix, params) => {
+  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+  var result11 = dotenv.config();
+  if (result11.error) {
+    console.log(result11.error, "<<<ERROR ENCOUNTER");
+  }
+  console.log(result11.parsed, "<<<Result Present");
+  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
   const result = await Axios.post(serverUrl + suffix, params);
   const { data = [] } = result || {};
   if (data.error) {
