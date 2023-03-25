27e1ce2f5ebef606eab914565e5283a40aacb31e
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-03-25 10:52:16 +0530

    Initial commit from Create Next App

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
   experimental: {
+    scrollRestoration: true,
     appDir: true,
   },
+  serverRuntimeConfig: {},
+  env: {},
 }
 
 module.exports = nextConfig
