261d304b461b433f62d29ea4c8067f5e85917fcf
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-04-23 14:51:29 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  webpack: (config) => {
+    // config.resolve.modules.push(path.resolve("./"));
+    // Unset client-side javascript that only works server-side
+    config.resolve.fallback = {
+      fs: false,
+      module: false,
+      child_process: false,
+    };
+    return config;
+  },
   env: {
     REACT_APP_SITE_URL:
       process.env.REACT_APP_SITE_URL || "http://localhost:3002",
--- pages/login.tsx
+++ pages/login.tsx
@@ -27,7 +27,7 @@ const Login = () => {
   }, [user]);
   return (
     <div className="container-fluid h-custom">
-      <Button />
+      {/* <Button /> */}
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
           {error && (
